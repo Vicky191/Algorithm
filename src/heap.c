71bb445d4b3f6409cad8f90c4be98f0b646e3c0b
Vicky191 <deependrasingh191@gmail.com>
2015-07-01 23:16:49 +0530

    Heap Imp

---
+++ src/heap.c
@@ -0,0 +1,21 @@
+/*
+ * heap.c
+ *
+ *  Created on: 01-Jul-2015
+ *      Author: Vicky
+ */
+int heap[100],heapSize;
+void init()
+{
+	heap[0]=-100;
+	heapSize=0;
+}
+
+void insert(int element)
+{
+
+}
+
+int extractMin()
+{}
+
